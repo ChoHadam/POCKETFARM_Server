27524715e001ed0241b89a905195aaa41662467d
ChoHadam <hadamcho97@gmail.com>
2020-07-19 15:34:30 +0900

    예약 전체 조회 기능 구현

--- pocketfarm_server/model/mypage.js
+++ pocketfarm_server/model/mypage.js
@@ -1,5 +1,7 @@
 const pool = require('../module/db/pool');
 const table1 = 'User';
+const table2 = 'Reservation';
+const table3 = 'Board';
 
 module.exports = {
     readInfo: async(userIdx) => {
@@ -7,4 +9,9 @@ module.exports = {
         const result = await pool.queryParam_None(`SELECT ${fields} FROM ${table1} WHERE userIdx = ${userIdx}`)
         return result;
     },
+    readMyReserve: async(userIdx) => {
+        const fields = 'reserveIdx, Reservation.boardIdx, title, boardImg, progressRate, remainPeriod';
+        const result = await pool.queryParam_None(`SELECT ${fields} FROM ${table2} JOIN ${table3} ON ${table2}.boardIdx = ${table3}.boardIdx WHERE Reservation.userIdx = ${userIdx}`)
+        return result;
+    }
 };
--- pocketfarm_server/routes/Mypage/index.js
+++ pocketfarm_server/routes/Mypage/index.js
@@ -28,4 +28,28 @@ router.get("/:userIdx", async(req, res) => {
     res.status(statusCode.OK).send(utils.successTrue(statusCode.OK,responseMessage.MYPAGE_READ_SUCCESS, result[0]));
 });
 
+// 마이페이지 조회
+router.get("/reservationAll/:userIdx", async(req, res) => {
+    const userIdx = req.params.userIdx;
+
+    if(!userIdx)
+    {
+    res.status(statusCode.BAD_REQUEST).send(utils.successFalse(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+    return;
+    }
+
+    var result = await Mypage.readMyReserve(userIdx);
+    console.log(result)
+
+    if(result.length == 0)
+    {
+    res.status(statusCode.INTERNAL_SERVER_ERROR).send(utils.successFalse(statusCode.INTERNAL_SERVER_ERROR, responseMessage.RESERVATION_READ_ALL_FAIL));
+    return;
+    }
+
+    res.status(statusCode.OK).send(utils.successTrue(statusCode.OK,responseMessage.RESERVATION_READ_ALL_SUCCESS, result));
+});
+
+
+
 module.exports = router;
